446c9fa639a8b1f72d0d542f933692d70f2e928a
Ed J <mohawk2@users.noreply.github.com>
2018-04-30 04:11:53 +0100

    quote-protect dirs

--- F77.pm
+++ F77.pm
@@ -108,7 +108,7 @@ $F77config{MinGW}{G77}{Link} = sub {
       $dir = "/usr/local/lib";
       $lib = "f2c";
    }  
-   return( "-L$dir -L/usr/lib -l$lib -lm" );
+   return( "\"-L$dir\" -L/usr/lib -l$lib -lm" );
 };
 
 $F77config{MinGW}{GFortran}{Link} = sub {
@@ -123,7 +123,7 @@ $F77config{MinGW}{GFortran}{Link} = sub {
    } else {
       $dir = "/usr/local/lib";
    }    
-   return( "-L$dir -L/usr/lib -lgfortran -lquadmath -lm" );
+   return( "\"-L$dir\" -L/usr/lib -lgfortran -lquadmath -lm" );
 };
 
 $F77config{MinGW}{G77}{Trail_} = 1;
@@ -142,7 +142,7 @@ $F77config{Sunos}{F77}{Link} = sub {
    $dir = find_highest_SC("/usr/lang/SC*");
    return "" unless $dir; # Failure
    print "$Pkg: Found Fortran latest version lib dir $dir\n";
-   return "-L$dir -lF77 -lm";
+   return "\"-L$dir\" -lF77 -lm";
 };
 
 # Whether symbols (subroutine names etc.) have trailing underscores 
@@ -244,7 +244,7 @@ $F77config{Solaris}{F77}{Link} = sub {
       /;
    }
 
-   join( ' ', "-L$dir", @libs );
+   join( ' ', qq{"-L$dir"}, @libs );
 };
 
 
@@ -361,7 +361,7 @@ $F77config{Freebsd}{G77}{Link} = sub {
     } else {
         $dir = "/usr/local/lib";
     }    
-    return( "-L$dir -L/usr/lib -lg2c -lm" );
+    return( "\"-L$dir\" -L/usr/lib -lg2c -lm" );
 };
 
 $F77config{Freebsd}{GFortran}{Link} = sub {
@@ -376,7 +376,7 @@ $F77config{Freebsd}{GFortran}{Link} = sub {
     } else {
         $dir = "/usr/local/lib";
     }    
-    return( "-L$dir -L/usr/lib -lgfortran -lm" );
+    return( "\"-L$dir\" -L/usr/lib -lgfortran -lm" );
 };
 
 $F77config{Freebsd}{G77}{Trail_} = 1;
@@ -702,7 +702,7 @@ sub gcclibs {
       unless($fallback_compiler eq 'GFortran' && $^O =~ /MSWin/i) {
          $gccdir = `$gcc -m32 -print-libgcc-file-name`; chomp $gccdir;
          $gccdir =~ s/\/libgcc.a//;
-         return " -L$gccdir -lgcc";
+         return " \"-L$gccdir\" -lgcc";
       }else{
          return "";
       } 
@@ -789,9 +789,9 @@ sub link_gnufortran_compiler {
    if ( $Config{osname} =~ /darwin/ && $Config{osvers} >= 14
       && $compiler eq 'gfortran' && $version >= 4.9 ) { # Add extra libs for gfortran versions >= 4.9 and OS X
       $append = "-lgcc_ext.10.5 -lgcc_s.10.5 -lquadmath";
-      return( "-L$dir $append -L/usr/lib -l$lib -lm" );
+      return( "\"-L$dir\" $append -L/usr/lib -l$lib -lm" );
    }
-   return( "-L$dir -L/usr/lib -l$lib -lm" );
+   return( "\"-L$dir\" -L/usr/lib -l$lib -lm" );
 }
 
 
